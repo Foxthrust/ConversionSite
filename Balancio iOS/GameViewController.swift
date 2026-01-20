681fe96eca3febed2435d7e161cf2ab1a7eb04ee
Ferdie Correa <ferdie.cm@icloud.com>
2026-01-19 19:45:00 -0600

    Initial Commit

---
+++ Balancio iOS/GameViewController.swift
@@ -0,0 +1,39 @@
+//
+//  GameViewController.swift
+//  Balancio iOS
+//
+//  Created by Ferdie Correa on 1/19/26.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene.newGameScene()
+
+        // Present the scene
+        let skView = self.view as! SKView
+        skView.presentScene(scene)
+        
+        skView.ignoresSiblingOrder = true
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
---
+++ Balancio macOS/GameViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GameViewController.swift
+//  Balancio macOS
+//
+//  Created by Ferdie Correa on 1/19/26.
+//
+
+import Cocoa
+import SpriteKit
+import GameplayKit
+
+class GameViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene.newGameScene()
+        
+        // Present the scene
+        let skView = self.view as! SKView
+        skView.presentScene(scene)
+        
+        skView.ignoresSiblingOrder = true
+        
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+    }
+
+}
+
